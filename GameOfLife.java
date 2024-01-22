6fa184eac276a88d980e8580d7895482aa2db90c
ShovalBiadgilin <Shoval.biadgilin@post.runi.ac.il>
2024-01-22 21:28:26 +0200

    Add awesome code for Homework 5

--- GameOfLife.java
+++ GameOfLife.java
@@ -146,11 +146,12 @@ public class GameOfLife {
 		int [][] board=arr;
 		for (int i=1; i<(board.length-1);i++){
 			for (int j=1; j<(board[i].length-1);j++){ 
-			System.out.printf("%2d", board[i][j]);
+			System.out.printf("%4d", board[i][j]);
 			}
 			System.out.println();
 		}
 	}
+	
 		
     // Displays the board. Living and dead cells are represented by black and white squares, respectively.
     // We use a fixed-size canvas of 900 pixels by 900 pixels for displaying game boards of different sizes.
